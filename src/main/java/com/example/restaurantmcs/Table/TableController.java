267a368f8b5ca0d79b51e385ab9266b61dae5e9b
Ekhorn <44117179+Ekhorn@users.noreply.github.com>
2021-11-25 05:51:57 +0100

    removed cross-origin annotation since their not needed anymore

--- src/main/java/com/example/restaurantmcs/Table/TableController.java
+++ src/main/java/com/example/restaurantmcs/Table/TableController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
-@CrossOrigin
 @RestController
 @RequestMapping("api/v1/table")
 public class TableController {
