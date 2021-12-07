e49cc009fe10cbccd5eff82f94eba247f66b7d46
DirkLemmen <33750270+DirkLemmen@users.noreply.github.com>
2021-12-07 14:15:46 +0100

    added unit tests for ingriedients

---
+++ src/test/java/com/example/restaurantmcs/Ingredients/IngredientServiceUnitTest.java
@@ -0,0 +1,97 @@
+package com.example.restaurantmcs.Ingredients;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import javax.persistence.EntityExistsException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class IngredientServiceUnitTest {
+
+    @Mock
+    private IngredientRepository ingredientRepository ;
+    @InjectMocks
+    private IngredientService underTest;
+
+    @BeforeEach
+    void setUp() {underTest = new IngredientService(ingredientRepository);}
+
+    @Test
+    void canGetIngredients() {
+        underTest.getIngredients();
+        verify(ingredientRepository).findAll();
+    }
+
+    @Test
+    void createIngredient() {
+        Ingredient ingredient = new Ingredient(1,"Paprika",true);
+
+        underTest.createIngredient(ingredient);
+
+        ArgumentCaptor<Ingredient> dishArgumentCaptor =
+                ArgumentCaptor.forClass(Ingredient.class);
+        verify(ingredientRepository)
+                .save(dishArgumentCaptor.capture());
+
+        Ingredient actual = dishArgumentCaptor.getValue();
+
+        // then
+        assertThat(actual).isEqualTo(ingredient);
+    }
+
+    @Test
+    void willThrowWhenIngredientNameIsTakenOnCreate() {
+        // given
+        Ingredient ingredient = new Ingredient(1,"Paprika",true);
+
+
+        given(ingredientRepository.findIngredientByName(anyString()))
+                .willReturn(java.util.Optional.of(ingredient));
+        // when
+        // then
+        assertThatThrownBy(() -> underTest.createIngredient(ingredient))
+                .isInstanceOf(EntityExistsException.class)
+                .hasMessageContaining("Ingredient already exist!");
+        verify(ingredientRepository, never()).save(any());
+    }
+
+    @Test
+    void canRemoveIngredient() {
+        // given
+        int id = 42;
+
+        given(ingredientRepository.existsById(id)).willReturn(true);
+        // when
+        // then
+        underTest.removeIngredient(id);
+        verify(ingredientRepository).deleteIngredientById(id);
+    }
+
+    @Test
+    void willThrownWhenIdDoesNotExistOnRemove()
+    {
+        // given
+        int id = 42;
+
+        given(ingredientRepository.existsById(id)).willReturn(false);
+
+        assertThatThrownBy(() -> underTest.removeIngredient(id))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessageContaining("Ingredient with id " + id + " does not exists!");
+    }
+}
