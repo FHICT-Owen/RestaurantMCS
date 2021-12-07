84c6a98224b548cdaee492ad3d7c7b523a02814a
DirkLemmen <33750270+DirkLemmen@users.noreply.github.com>
2021-12-07 14:33:00 +0100

    Create IngredientControllerIntegrationTest.java

---
+++ src/test/java/com/example/restaurantmcs/Ingredients/IngredientControllerIntegrationTest.java
@@ -0,0 +1,81 @@
+package com.example.restaurantmcs.Ingredients;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(IngredientController.class)
+class IngredientControllerIntegrationTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    IngredientService ingredientService;
+
+    @Test
+    void shouldReturnIngredients() throws Exception {
+        Ingredient ingredient1 = new Ingredient(1, "Paprika", true);
+        Ingredient ingredient2 = new Ingredient(2, "Oregano", false);
+
+        List<Ingredient> ingredientList = new ArrayList<>();
+        ingredientList.add(ingredient1);
+        ingredientList.add(ingredient2);
+
+        when(ingredientService.getIngredients()).thenReturn(ingredientList);
+
+        mockMvc.perform(get("/api/v1/ingredient"))
+                .andDo(print()).andExpect(status().isOk()).andExpect(content().json(convertObjectToJsonString(ingredientList)));
+    }
+
+    @Test
+    void shouldAddNewIngredient() throws Exception {
+        Ingredient ingredient = new Ingredient(1, "Paprika", true);
+
+        mockMvc.perform(post("/api/v1/ingredient")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(convertObjectToJsonString(ingredient)))
+                .andDo(print()).andExpect(status().isOk());
+    }
+
+    @Test
+    void shouldDeleteIngredient() throws Exception {
+        mockMvc.perform(delete("/api/v1/ingredient/{id}", 1))
+                .andDo(print()).andExpect(status().isOk());
+    }
+
+    private String convertObjectToJsonString(List<Ingredient> list) {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            return mapper.writeValueAsString(list);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            throw new RuntimeException();
+        }
+    }
+
+    private String convertObjectToJsonString(Ingredient item) {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            return mapper.writeValueAsString(item);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            throw new RuntimeException();
+        }
+    }
+}
